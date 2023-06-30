7f81d27e09d8e072856ec6e6ce432883ba592aa6
scffs <khidey@inbox.ru>
2023-06-30 16:00:33 +0700

    Added swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,49 @@
 import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiProperty, ApiOkResponse, ApiNotFoundResponse } from '@nestjs/swagger';
 import { RateLimit } from 'nestjs-rate-limiter';
 
 import { UsersService } from './user.service';
 import { User } from './user.model';
 import { CreateUserDto } from './dto/create-user.dto';
 
+class UserExistsResponse {
+  @ApiProperty({ type: String })
+  message: string;
+
+  @ApiProperty({ type: Number })
+  statusCode: number;
+}
+
+class CollectionsResponse {
+  @ApiProperty({ type: [String] })
+  collections: string[];
+}
+
 @Controller('users')
+@ApiTags('Users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiOkResponse({
+    description: 'Returns an array of users',
+    type: User,
+    isArray: true,
+  })
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a user' })
+  @ApiProperty({
+    type: CreateUserDto,
+  })
+  @ApiOkResponse({
+    description: 'User successfully created',
+    type: User,
+  })
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
@@ -26,12 +55,40 @@ export class UsersController {
     errorMessage: 'Too many uploads',
   })
   @Get('/check-user')
-  async checkUser(@Query('_id') userId: string): Promise<{ message: string, statusCode: number }> {
+  @ApiOperation({ summary: 'Check if a user exists' })
+  @ApiProperty({
+    type: String,
+    name: '_id',
+    required: true,
+    description: 'User ID',
+  })
+  @ApiOkResponse({
+    description: 'User already exists',
+    type: UserExistsResponse,
+    status: 200
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found',
+    type: UserExistsResponse,
+    status: 404
+  })
+  async checkUser(@Query('_id') userId: string): Promise<UserExistsResponse> {
     return this.usersService.checkUser(userId);
   }
 
   @Get('/collections')
-  async getCollections(): Promise<{ collections: string[] }> {
+  @ApiOperation({ summary: 'Get collections' })
+  @ApiOkResponse({
+    description: 'Returns an array of collection names',
+    type: CollectionsResponse,
+    status: 200,
+    schema: {
+      example: {
+        collections: ['collection1', 'collection2', 'collection3']
+      }
+    }
+  })
+  async getCollections(): Promise<CollectionsResponse> {
     return this.usersService.getCollections();
   }
 }
