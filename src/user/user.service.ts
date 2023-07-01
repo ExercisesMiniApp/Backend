a92c1c1844a50700385fee17edae48b46b1459d5
scffs <khidey@inbox.ru>
2023-07-01 22:30:12 +0700

    New funcs

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,31 +1,34 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
 
+import { CreateUserDto, UserResponse } from './dto';
 import { User } from './user.model';
-import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
+    private jwtService: JwtService,
   ) {}
+
+  async findOne(_id: number): Promise<User | undefined> {
+    return this.userModel.findOne({ _id }).exec();
+  }
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
   }
 
-  async create(user: CreateUserDto): Promise<User> {
+  async create(user: CreateUserDto): Promise<string> {
     const createdUser = new this.userModel(user);
-    return createdUser.save();
+    const savedUser = await createdUser.save();
+
+    return await this.createToken(savedUser);
   }
 
-  async checkUser(userId: string): Promise<{ message: string, statusCode: number }> {
+  async checkUser(userId: string): Promise<UserResponse> {
     if (!userId) {
       throw new HttpException('No ID', HttpStatus.BAD_REQUEST);
     }
@@ -39,7 +42,11 @@ export class UsersService {
     const existingUser = await this.userModel.findOne({ _id: userId }).exec();
 
     if (existingUser) {
-      throw new HttpException('User already exists', HttpStatus.OK);
+      return {
+        message: 'User found',
+        statusCode: HttpStatus.OK,
+        token: await this.createToken(existingUser),
+      };
     } else {
       throw new NotFoundException('User not found');
     }
@@ -49,5 +56,10 @@ export class UsersService {
     const collections = await this.userModel.db.db.listCollections().toArray();
     const collectionNames: string[] = collections.map((collection) => collection.name);
     return { collections: collectionNames };
+  }
+
+  private async createToken(user: User): Promise<string> {
+    const payload = { sub: user._id };
+    return await this.jwtService.signAsync(payload);
   }
 }
